653709a73c7e316033275dc4eb664d2f67a521c9
guananya <93958307+guananya@users.noreply.github.com>
2022-06-14 17:28:25 +0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  let c = createCanvas(256, 256);
+  background('lavender');
+  //it is 16x16 grid. Each square is 16 in length
+  for (i = 0; i<256;) {
+    for (n = 0; n<256;) {
+      //image(img, i, n, 16, 16);
+      fill('powderblue')
+      square(i, n, 16);
+      n += 16;
+    }
+    i += 16;
+  }
+  
+  saveCanvas(c, 'myCanvas', 'png');
+  
+}
+
+function draw() {
+  //background(220);
+}
